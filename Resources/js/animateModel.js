2dd901025a1cbff1594de29e5f50b1fefebcdd4c
MikeTalalov <m.talalov@gmail.com>
2013-03-04 14:54:10 +0400

    123

--- Resources/js/animateModel.js
+++ Resources/js/animateModel.js
@@ -66,7 +66,7 @@ var animateLimb = function(p1, p2, p3, asset, _toTrace){
 		p3.move(x2,y2);
 	};
 	
-	setInterval(movement, 500);
+	setInterval(movement, 200);
 };
 
 exports.animateHead = animateHead;
